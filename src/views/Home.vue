2d35e2bdfa5ab92c577aa2c32564de28e5a4a4b4
juan pablo gz <juanpablogomezzapata@gmail.com>
2020-01-07 12:34:22 -0300

    'Initial
    commit'

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,87 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+  <div>
+      <v-layout>
+        <v-flex xs12>
+
+          <v-card>
+            <v-date-picker 
+            v-model="fecha"
+            full-width
+            locale= "es-cl"
+            :min ="minimo"
+            :max ="maximo"
+            @change="getDolar(fecha)"
+            >
+
+            </v-date-picker>
+          </v-card>
+
+        <v-card color="error" dark>
+         <v-card-text class="display-1 text-center">
+           {{valor}}
+         </v-card-text>
+        </v-card>
+        </v-flex>
+       
+       </v-layout>
+
+       
   </div>
+ 
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
+
+import  axios  from "axios";
+
 
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
+data() {
+  return {
+    fecha: new Date().toISOString().substr(0, 10),
+    minimo: "1984",
+    maximo: new Date().toISOString().substr(0, 10),
+    valor: null
   }
+},
+methods: {
+
+
+  async getDolar(dia){
+    console.log(dia);
+     let arrayFecha = dia.split(['-'])
+     console.log (arrayFecha);
+     let ddmmyy = arrayFecha[2] + '-' + arrayFecha[1] + '-' + arrayFecha[0]
+
+    try {
+
+    
+        let datos = await axios.get(`https://mindicador.cl/api/dolar/${ddmmyy}`)
+
+        if (datos.data.serie > 0) {
+          this.valor = await datos.data.serie[0].valor
+        } else {
+          this.valor = 'sin resultado'
+        }
+      console.log (datos.data.serie);
+      
+      this.valor = await datos.data.serie[0].valor
+    } catch (error) {
+      
+    }
+
+    finally{
+
+
+    }
+
+    
+  }
+},
+created() {
+  this.getDolar(this.fecha)
+}
 }
 </script>
